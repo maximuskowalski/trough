a8dc675a431b765bb622f4fa16b40c3cd63a16fd
Max Kowalski <13492750+maximuskowalski@users.noreply.github.com>
2020-09-25 13:55:28 +1000

    Update snout.sh
    ix9 fixed muh greps

--- snout.sh
+++ snout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-WATCHDIR=/mnt/unionfs/magazines
+WATCHDIR="/mnt/unionfs/magazines"
 WATCHTXT="(?i)(Travel|photography|Consumer)"
 WATCHFILE=/opt/scripts/trough/watch.txt
 WEBHOOKID=123456    #discord webhook id
@@ -10,15 +10,8 @@ WEBHOOKTOKEN=ABC123 #discord webhook token
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 WOOK=discord://${WEBHOOKID}/${WEBHOOKTOKEN}
 
-inotifywait -qme create --format '%f' ${WATCHDIR} | while read line; do
-  grep -P ${WATCHTXT} "$line" |
+inotifywait -m --format '%f' -e create -e moved_to "${WATCHDIR}" | while read f; do
+  if echo $f | grep -iP ${WATCHTXT}; then
     apprise -vv -t "**Magazine Delivery**" -b "$f" "${WOOK}"
+  fi
 done
-
-# inotifywait -qm --format '%f' -e create -e moved_to "${WATCHDIR}" | while read f; do
-#     if grep -i 'travel\|consumer\|photgraphy' "$f"; then
-#       echo "match $f"
-#     else
-#       echo "not match $f"
-#     fi
-#   done
